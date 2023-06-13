2ef2fb71426b53ed6671c74e3c62e302837d5fe8
Alberto Pasqualetto <alberto.pasqualetto.2@studenti.unipd.it>
2023-06-14 01:51:47 +0200

    Changed targetSdk accordingly to my main device
    and other small changes

--- app/src/main/java/it/albertopasqualetto/soundmeteresp/ui/theme/Theme.kt
+++ app/src/main/java/it/albertopasqualetto/soundmeteresp/ui/theme/Theme.kt
@@ -1,8 +1,6 @@
 package it.albertopasqualetto.soundmeteresp.ui.theme
 
 import android.app.Activity
-import android.os.Build
-import android.view.WindowInsetsController
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
@@ -14,7 +12,6 @@ import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 
 private val LightColors = lightColorScheme(
